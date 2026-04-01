96de6a6b9e13e609b48cc710ebdb34080dead880
joanna-marks <146840655+joanna-marks@users.noreply.github.com>
2026-04-01 10:26:29 +0000

    Deploying to gh-pages from @ joanna-marks/joanna-marks.github.io@1ec6ffbe460105c89db1372a97fe311980ebfe25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -26,6 +26,9 @@ ninja.data = [{
         },{id: "news-i-will-be-presenting-a-poster-at-amortized-probml-workshop-at-ellis-unconference-which-will-take-place-alongside-eurips-in-copenhagen-the-poster-will-be-a-summary-of-our-joint-work-with-tim-y-j-wang-and-deniz-akyildiz-on-leveraging-particle-system-for-efficient-training-of-energy-based-models-come-along-if-interested",
           title: 'I will be presenting a poster at Amortized ProbML workshop at ELLIS Unconference...',
           description: "",
+          section: "News",},{id: "news-gabriel-rioux-will-present-our-joint-work-on-gromov-wasserstein-distances-between-finite-spaces-duality-computation-and-entropic-approximation-with-riccardo-passeggeri-at-cdm-seminars-series-at-epfl-come-chat-to-him-if-interested-pre-print-coming-soon",
+          title: 'Gabriel Rioux will present our joint work on “Gromov-Wasserstein distances between finite spaces:...',
+          description: "",
           section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
